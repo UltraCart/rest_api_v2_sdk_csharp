cdc1713cc876ef5ce1a9e3d45822ea58b8070cb0
Perry Tew <perry@ultracart.com>
2022-07-25 14:49:22 -0400

    added createEmailSendingDomain2 to support additional MTAs

--- src/com.ultracart.admin.v2/Model/ConversationWebsocketMessage.cs
+++ src/com.ultracart.admin.v2/Model/ConversationWebsocketMessage.cs
@@ -126,7 +126,7 @@ namespace com.ultracart.admin.v2.Model
         /// <param name="eventUpdatedMessage">eventUpdatedMessage.</param>
         /// <param name="message">message.</param>
         /// <param name="type">Type of message.</param>
-        public ConversationWebsocketMessage(string conversationUuid = default(string), Conversation eventConversationClosed = default(Conversation), Conversation eventNewConversation = default(Conversation), ConversationMessage eventNewMessage = default(ConversationMessage), Object eventQueuePosition = default(Object), EventTypeEnum? eventType = default(EventTypeEnum?), ConversationMessage eventUpdatedMessage = default(ConversationMessage), ConversationMessage message = default(ConversationMessage), TypeEnum? type = default(TypeEnum?))
+        public ConversationWebsocketMessage(string conversationUuid = default(string), Conversation eventConversationClosed = default(Conversation), Conversation eventNewConversation = default(Conversation), Conversation eventNewMessage = default(Conversation), ConversationEventQueuePosition eventQueuePosition = default(ConversationEventQueuePosition), EventTypeEnum? eventType = default(EventTypeEnum?), ConversationMessage eventUpdatedMessage = default(ConversationMessage), ConversationMessage message = default(ConversationMessage), TypeEnum? type = default(TypeEnum?))
         {
             this.ConversationUuid = conversationUuid;
             this.EventConversationClosed = eventConversationClosed;
@@ -162,13 +162,13 @@ namespace com.ultracart.admin.v2.Model
         /// Gets or Sets EventNewMessage
         /// </summary>
         [DataMember(Name="event_new_message", EmitDefaultValue=false)]
-        public ConversationMessage EventNewMessage { get; set; }
+        public Conversation EventNewMessage { get; set; }
 
         /// <summary>
         /// Gets or Sets EventQueuePosition
         /// </summary>
         [DataMember(Name="event_queue_position", EmitDefaultValue=false)]
-        public Object EventQueuePosition { get; set; }
+        public ConversationEventQueuePosition EventQueuePosition { get; set; }
 
 
         /// <summary>

111642d8613e043f6e1248538a2f84947e19a4d8
Perry Tew <perry@ultracart.com>
2025-01-09 16:35:58 -0500

    automation test

--- src/com.ultracart.admin.v2/Properties/AssemblyInfo.cs
+++ src/com.ultracart.admin.v2/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.10.222")]
-[assembly: AssemblyFileVersion("3.10.222")]
+[assembly: AssemblyVersion("3.10.223")]
+[assembly: AssemblyFileVersion("3.10.223")]

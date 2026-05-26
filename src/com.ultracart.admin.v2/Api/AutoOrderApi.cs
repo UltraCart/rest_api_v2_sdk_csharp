a9fab0d1ba74ef7c69cb06eed1eeb9d826783609
Perry Tew <perry@ultracart.com>
2026-05-26 09:31:49 -0400

    AutoOrderApi.getAutoOrderCancelReasons

--- src/com.ultracart.admin.v2/Api/AutoOrderApi.cs
+++ src/com.ultracart.admin.v2/Api/AutoOrderApi.cs
@@ -170,6 +170,25 @@ namespace com.ultracart.admin.v2.Api
         /// <returns>ApiResponse of AutoOrderResponse</returns>
         ApiResponse<AutoOrderResponse> GetAutoOrderByReferenceOrderIdWithHttpInfo (string referenceOrderId, string expand = default(string));
         /// <summary>
+        /// Retrieve auto order cancel reasons.
+        /// </summary>
+        /// <remarks>
+        /// Retrieves auto order cancel reasons. 
+        /// </remarks>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <returns>AutoOrderCancelReasonsResponse</returns>
+        AutoOrderCancelReasonsResponse GetAutoOrderCancelReasons ();
+
+        /// <summary>
+        /// Retrieve auto order cancel reasons.
+        /// </summary>
+        /// <remarks>
+        /// Retrieves auto order cancel reasons. 
+        /// </remarks>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <returns>ApiResponse of AutoOrderCancelReasonsResponse</returns>
+        ApiResponse<AutoOrderCancelReasonsResponse> GetAutoOrderCancelReasonsWithHttpInfo ();
+        /// <summary>
         /// Retrieve email delivery information for this auto order.
         /// </summary>
         /// <remarks>
@@ -623,6 +642,27 @@ namespace com.ultracart.admin.v2.Api
         /// <param name="cancellationToken">Cancellation Token to cancel request (optional) </param>
         /// <returns>Task of ApiResponse (AutoOrderResponse)</returns>
         System.Threading.Tasks.Task<ApiResponse<AutoOrderResponse>> GetAutoOrderByReferenceOrderIdWithHttpInfoAsync (string referenceOrderId, string expand = default(string), CancellationToken cancellationToken = default(CancellationToken));
+        /// <summary>
+        /// Retrieve auto order cancel reasons.
+        /// </summary>
+        /// <remarks>
+        /// Retrieves auto order cancel reasons. 
+        /// </remarks>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="cancellationToken">Cancellation Token to cancel request (optional) </param>
+        /// <returns>Task of AutoOrderCancelReasonsResponse</returns>
+        System.Threading.Tasks.Task<AutoOrderCancelReasonsResponse> GetAutoOrderCancelReasonsAsync (CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// Retrieve auto order cancel reasons.
+        /// </summary>
+        /// <remarks>
+        /// Retrieves auto order cancel reasons. 
+        /// </remarks>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="cancellationToken">Cancellation Token to cancel request (optional) </param>
+        /// <returns>Task of ApiResponse (AutoOrderCancelReasonsResponse)</returns>
+        System.Threading.Tasks.Task<ApiResponse<AutoOrderCancelReasonsResponse>> GetAutoOrderCancelReasonsWithHttpInfoAsync (CancellationToken cancellationToken = default(CancellationToken));
         /// <summary>
         /// Retrieve email delivery information for this auto order.
         /// </summary>
@@ -2106,6 +2146,151 @@ namespace com.ultracart.admin.v2.Api
             return new ApiResponse<AutoOrderResponse>(localVarStatusCode,
                 localVarResponse.Headers.ToDictionary(x => x.Name, x => string.Join(",", x.Value)),
                 (AutoOrderResponse) this.Configuration.ApiClient.Deserialize(localVarResponse, typeof(AutoOrderResponse)));
+        }
+
+        /// <summary>
+        /// Retrieve auto order cancel reasons. Retrieves auto order cancel reasons. 
+        /// </summary>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <returns>AutoOrderCancelReasonsResponse</returns>
+        public AutoOrderCancelReasonsResponse GetAutoOrderCancelReasons ()
+        {
+             ApiResponse<AutoOrderCancelReasonsResponse> localVarResponse = GetAutoOrderCancelReasonsWithHttpInfo();
+             return localVarResponse.Data;
+        }
+
+        /// <summary>
+        /// Retrieve auto order cancel reasons. Retrieves auto order cancel reasons. 
+        /// </summary>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <returns>ApiResponse of AutoOrderCancelReasonsResponse</returns>
+        public ApiResponse<AutoOrderCancelReasonsResponse> GetAutoOrderCancelReasonsWithHttpInfo ()
+        {
+
+            var localVarPath = "/auto_order/auto_orders/cancel_reasons";
+            var localVarPathParams = new Dictionary<String, String>();
+            var localVarQueryParams = new List<KeyValuePair<String, String>>();
+            var localVarHeaderParams = new Dictionary<String, String>(this.Configuration.DefaultHeader);
+            var localVarFormParams = new Dictionary<String, String>();
+            var localVarFileParams = new Dictionary<String, FileParameter>();
+            Object localVarPostBody = null;
+
+            // to determine the Content-Type header
+            String[] localVarHttpContentTypes = new String[] {
+            };
+            String localVarHttpContentType = this.Configuration.ApiClient.SelectHeaderContentType(localVarHttpContentTypes);
+
+            // to determine the Accept header
+            String[] localVarHttpHeaderAccepts = new String[] {
+                "application/json"
+            };
+            String localVarHttpHeaderAccept = this.Configuration.ApiClient.SelectHeaderAccept(localVarHttpHeaderAccepts);
+            if (localVarHttpHeaderAccept != null)
+                localVarHeaderParams.Add("Accept", localVarHttpHeaderAccept);
+
+
+            // authentication (ultraCartOauth) required
+            // oauth required
+            if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
+            {
+                localVarHeaderParams["Authorization"] = "Bearer " + this.Configuration.AccessToken;
+            }
+            // authentication (ultraCartSimpleApiKey) required
+            if (!String.IsNullOrEmpty(this.Configuration.GetApiKeyWithPrefix("x-ultracart-simple-key")))
+            {
+                localVarHeaderParams["x-ultracart-simple-key"] = this.Configuration.GetApiKeyWithPrefix("x-ultracart-simple-key");
+            }
+
+            // make the HTTP request
+            IRestResponse localVarResponse = (IRestResponse) this.Configuration.ApiClient.CallApi(localVarPath,
+                Method.GET, localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarFileParams,
+                localVarPathParams, localVarHttpContentType);
+
+            int localVarStatusCode = (int) localVarResponse.StatusCode;
+
+            if (ExceptionFactory != null)
+            {
+                Exception exception = ExceptionFactory("GetAutoOrderCancelReasons", localVarResponse);
+                if (exception != null) throw exception;
+            }
+
+            return new ApiResponse<AutoOrderCancelReasonsResponse>(localVarStatusCode,
+                localVarResponse.Headers.ToDictionary(x => x.Name, x => string.Join(",", x.Value)),
+                (AutoOrderCancelReasonsResponse) this.Configuration.ApiClient.Deserialize(localVarResponse, typeof(AutoOrderCancelReasonsResponse)));
+        }
+
+        /// <summary>
+        /// Retrieve auto order cancel reasons. Retrieves auto order cancel reasons. 
+        /// </summary>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="cancellationToken">Cancellation Token to cancel request (optional) </param>
+        /// <returns>Task of AutoOrderCancelReasonsResponse</returns>
+        public async System.Threading.Tasks.Task<AutoOrderCancelReasonsResponse> GetAutoOrderCancelReasonsAsync (CancellationToken cancellationToken = default(CancellationToken))
+        {
+             ApiResponse<AutoOrderCancelReasonsResponse> localVarResponse = await GetAutoOrderCancelReasonsWithHttpInfoAsync(cancellationToken);
+             return localVarResponse.Data;
+
+        }
+
+        /// <summary>
+        /// Retrieve auto order cancel reasons. Retrieves auto order cancel reasons. 
+        /// </summary>
+        /// <exception cref="com.ultracart.admin.v2.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="cancellationToken">Cancellation Token to cancel request (optional) </param>
+        /// <returns>Task of ApiResponse (AutoOrderCancelReasonsResponse)</returns>
+        public async System.Threading.Tasks.Task<ApiResponse<AutoOrderCancelReasonsResponse>> GetAutoOrderCancelReasonsWithHttpInfoAsync (CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            var localVarPath = "/auto_order/auto_orders/cancel_reasons";
+            var localVarPathParams = new Dictionary<String, String>();
+            var localVarQueryParams = new List<KeyValuePair<String, String>>();
+            var localVarHeaderParams = new Dictionary<String, String>(this.Configuration.DefaultHeader);
+            var localVarFormParams = new Dictionary<String, String>();
+            var localVarFileParams = new Dictionary<String, FileParameter>();
+            Object localVarPostBody = null;
+
+            // to determine the Content-Type header
+            String[] localVarHttpContentTypes = new String[] {
+            };
+            String localVarHttpContentType = this.Configuration.ApiClient.SelectHeaderContentType(localVarHttpContentTypes);
+
+            // to determine the Accept header
+            String[] localVarHttpHeaderAccepts = new String[] {
+                "application/json"
+            };
+            String localVarHttpHeaderAccept = this.Configuration.ApiClient.SelectHeaderAccept(localVarHttpHeaderAccepts);
+            if (localVarHttpHeaderAccept != null)
+                localVarHeaderParams.Add("Accept", localVarHttpHeaderAccept);
+
+
+            // authentication (ultraCartOauth) required
+            // oauth required
+            if (!String.IsNullOrEmpty(this.Configuration.AccessToken))
+            {
+                localVarHeaderParams["Authorization"] = "Bearer " + this.Configuration.AccessToken;
+            }
+            // authentication (ultraCartSimpleApiKey) required
+            if (!String.IsNullOrEmpty(this.Configuration.GetApiKeyWithPrefix("x-ultracart-simple-key")))
+            {
+                localVarHeaderParams["x-ultracart-simple-key"] = this.Configuration.GetApiKeyWithPrefix("x-ultracart-simple-key");
+            }
+
+            // make the HTTP request
+            IRestResponse localVarResponse = (IRestResponse) await this.Configuration.ApiClient.CallApiAsync(localVarPath,
+                Method.GET, localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarFileParams,
+                localVarPathParams, localVarHttpContentType, cancellationToken);
+
+            int localVarStatusCode = (int) localVarResponse.StatusCode;
+
+            if (ExceptionFactory != null)
+            {
+                Exception exception = ExceptionFactory("GetAutoOrderCancelReasons", localVarResponse);
+                if (exception != null) throw exception;
+            }
+
+            return new ApiResponse<AutoOrderCancelReasonsResponse>(localVarStatusCode,
+                localVarResponse.Headers.ToDictionary(x => x.Name, x => string.Join(",", x.Value)),
+                (AutoOrderCancelReasonsResponse) this.Configuration.ApiClient.Deserialize(localVarResponse, typeof(AutoOrderCancelReasonsResponse)));
         }
 
         /// <summary>
